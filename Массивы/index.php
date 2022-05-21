0c2b07c16b31e6149534af703fd4faa9489b2ec1
Timofey solovov <timoshA228@228.com>
2022-05-22 00:00:11 +0300

    new

---
+++ Массивы/index.php
@@ -0,0 +1,24 @@
+<h1>
+	<?php
+	$macc1 = [2020,"Ivan",False];
+	$macc2 =[
+	["name"=>"DEV","surname"=>"ops","age"=>24,"id"=>13,"lang"=>"rus"],
+	["name"=>"DEV","surname"=>"ops","age"=>24,"id"=>13,"lang"=>"rus"],
+	["name"=>"DEV","surname"=>"ops","age"=>24,"id"=>13,"lang"=>"rus"],
+	];
+	$db=[
+	"port"=>8080,
+	"Host"=>"localhost",
+	"login"=>"vas228",
+	"password"=>"pass",
+	];
+	$macc3 = [
+	'Key'=>"r",
+	'URl'=>8,
+	"ssl"=>'Cto-to',
+	"db"=>"ver5.1",
+	$db,
+	];
+	print_r ($macc3);
+	?>
+</h1>
